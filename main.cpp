653359135190284429c77cb16276be6167c31637
VodoGamer <vodogamer@yandex.ru>
2024-06-11 12:26:06 +0300

    update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@
 
 #include "list_object.cpp"
 
+void print_file(string);
+void print_main_menu(List *);
+void print_menu_options(List *);
+void create_new_list(List *);
+
+int main()
+{
+  List *lists = nullptr;
+  system("clear");
+  print_main_menu(lists);
+  return 0;
+}
+
 void print_file(string file_name)
 {
   std::ifstream file;
@@ -19,9 +32,53 @@ void print_file(string file_name)
   file.close();
 }
 
+void create_new_list(List *lists)
+{
+  std::cout << "Создание нового списка" << endl;
+  List *list = new List();
+  string choice;
+  while (choice != "x")
+  {
+    std::cout << "Введите новый элемент списка (x для выхода): ";
+    std::cin >> choice;
+    if (choice != "x")
+      list->append(std::stoi(choice));
+  };
+  lists = list;
+  system("clear");
+  print_main_menu(lists);
+}
+
 void print_main_menu(List *lists)
 {
   print_file("titles/hello.txt");
+  print_menu_options(lists);
+  int choice = 0;
+  std::cout << ">> ";
+  std::cin >> choice;
+  switch (choice)
+  {
+  case 1:
+    create_new_list(lists);
+    break;
+  case 2:
+    system("clear");
+    delete lists;
+    lists = nullptr;
+    print_main_menu(lists);
+    break;
+  case 3:
+    system("clear");
+    lists->print();
+    print_main_menu(lists);
+    break;
+  default:
+    break;
+  }
+}
+
+void print_menu_options(List *lists)
+{
   if (lists == nullptr)
   {
     std::cout << "У вас нет списков на данный момент" << endl;
@@ -30,15 +87,7 @@ void print_main_menu(List *lists)
   if (lists != nullptr)
   {
     std::cout << "[2] Удалить список" << endl;
+    std::cout << "[3] Вывести список" << endl;
   }
-}
-
-int main()
-{
-  List *lists = nullptr;
-  print_main_menu(lists);
-  int choice = 0;
-  std::cout << ">> ";
-  std::cin >> choice;
-  return 0;
+  std::cout << "[0] Выход" << endl;
 }
